dd19a049bbcdf08901b38f20962a6463437417d1
NeoDigi <julien@crowdcast.jp>
2019-01-31 12:54:01 +0900

    upgrade to dart2

--- lib/src/parameters.dart
+++ lib/src/parameters.dart
@@ -15,47 +15,47 @@ class Parameters {
   var _query = "";
 
   Parameters addFilter(String filter) {
-    _filters.add(("&filter" + filter).replaceAll(new RegExp(" "), ""));
+    _filters.add(("&filter" + filter).replaceAll(RegExp(" "), ""));
     return this;
   }
 
   Parameters addIds(String ids) {
-    _ids = ids.replaceAll(new RegExp(" "), "");
+    _ids = ids.replaceAll(RegExp(" "), "");
     return this;
   }
 
   Parameters addOffset(String offset) {
-    _offset = "&offset=" + offset.replaceAll(new RegExp(" "), "");
+    _offset = "&offset=" + offset.replaceAll(RegExp(" "), "");
     return this;
   }
 
   Parameters addFields(String fields) {
-    _fields = fields.replaceAll(new RegExp(" "), "");
+    _fields = fields.replaceAll(RegExp(" "), "");
     return this;
   }
 
   Parameters addExpand(String expand) {
-    _expand = "&expand=" + expand.replaceAll(new RegExp(" "), "");
+    _expand = "&expand=" + expand.replaceAll(RegExp(" "), "");
     return this;
   }
 
   Parameters addLimit(String limit) {
-    _limit = "&limit=" + limit.replaceAll(new RegExp(" "), "");
+    _limit = "&limit=" + limit.replaceAll(RegExp(" "), "");
     return this;
   }
 
   Parameters addOrder(String order) {
-    _order = "&order=" + order.replaceAll(new RegExp(" "), "");
+    _order = "&order=" + order.replaceAll(RegExp(" "), "");
     return this;
   }
 
   Parameters addSearch(String search) {
-    _search = "?search=" + search.replaceAll(new RegExp(" "), "%20");
+    _search = "?search=" + search.replaceAll(RegExp(" "), "%20");
     return this;
   }
 
   Parameters addScroll(String scroll) {
-    _scroll = "&scroll=" + scroll.replaceAll(new RegExp(" "), "");
+    _scroll = "&scroll=" + scroll.replaceAll(RegExp(" "), "");
     return this;
   }
 
